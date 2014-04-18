2acc8f575f05072e6d216dc29dd6c78c3de922c7
mnicole <michele.larson@gmail.com>
2014-04-17 17:11:36 -0700

    flip

---
+++ js/flip.js
@@ -0,0 +1,8 @@
+function changeClass(){
+  if(document.getElementById("block").className == "block") {
+    document.getElementById("block").className += " rotated";
+  }
+  else {
+      document.getElementById("block").className = "block";
+  }
+};
