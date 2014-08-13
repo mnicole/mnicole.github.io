43bdac734f2387a4e6c8416b11620f2f3a383df2
Michele <michele@card.com>
2014-08-12 23:16:01 -0700

    new site using reveal.js

--- js/flip.js
+++
@@ -1,8 +0,0 @@
-function changeClass(){
-  if(document.getElementById("block").className == "block") {
-    document.getElementById("block").className += " rotated";
-  }
-  else {
-      document.getElementById("block").className = "block";
-  }
-};
